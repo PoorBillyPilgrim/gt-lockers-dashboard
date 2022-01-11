922ed3563a3a9bf10240c24fde32d22f967d29f0
Tyler Jones <tjones357@gatech.edu>
2022-01-11 16:07:35 -0500

    basic database connection

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
-require('dotenv').config()
 const express = require('express')
+const mysql = require('mysql2')
+const config = require('./config.js')
 
 const cors = require('cors')
+const helmet = require('helmet')
 
 const app = express()
-const port = process.env.PORT || 4000
+const port = config.port || 4000
 
 app.use(cors())
+app.use(helmet())
+
+
+const pool = mysql.createPool(config.db)
+
 
 
 app.get('/', (req, res) => {
-    res.send('hello world')
-})
-
-app.get('/hello', (req, res) => {
-    res.send('hello WORLD')
+  pool.getConnection((err, connection) => {
+      if (err) console.error(err)
+      connection.query('SELECT * FROM lockers WHERE floor = 5', (err, results, fields) => {
+          if (err) console.error(err)
+          res.send(results)
+      })
+  })
 })
 
 app.listen(port, () => {
-    console.log(`server listening on ${port}`)
+  console.log(`server listening on ${port}`)
 })
