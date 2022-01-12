b99b68fdfda2f9b8557506c1af710c577b456067
Tyler Jones <tjones357@gatech.edu>
2022-01-12 11:51:50 -0500

    basic error handling

--- server.js
+++ server.js
@@ -12,30 +12,23 @@ app.use(cors())
 app.use(helmet())
 
 app.get('/', async (req, res) => {
-  
+	
 	try {
 		const pool = await mysql.createPool(config.db)
 		const connection = await pool.getConnection()
-		const [rows] = await connection.execute('SELECT * FROM lockers WHERE floor = ?', [5])
-		res.status(200).send(rows)
-		await connection.release()
+		try {
+			const [rows] = await connection.execute('SELECT * FROM lockers WHERE floor = ?', [5])
+			res.status(200).send(rows)
+		} catch (err) {
+			res.status(300).json({'error':'sorry, a query error has occurred'})
+			console.error(err)
+		} finally {
+			connection.release()
+		}
 	} catch (err) {
-		res.status(300).json({'error':'sorry, an error has occured'})
+		res.status(300).json({'error': 'sorry, a connection error has occurred'})
 		console.error(err)
 	}
-	
-	/*pool.getConnection((err, connection) => {
-    if (err) console.error(err)
-		// .execute creates a prepared statement
-    connection.execute('SELECT * FROM lockers WHERE floor = ?', [5], (err, results) => {
-      if (err) {
-				res.status(300).json({'error':'sorry, an error has occured'})
-				console.error(err)
-			}
-      res.status(200).send(results )
-    })
-    pool.releaseConnection(connection)
-  })*/
 })
 
 app.listen(port, () => {
