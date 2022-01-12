151ac4a4d883836f0b361c2da9b87b00d035273c
Tyler Jones <tjones357@gatech.edu>
2022-01-12 10:58:32 -0500

    test async mysql connection

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const mysql = require('mysql2')
+const mysql = require('mysql2/promise')
 const config = require('./config.js')
 
 const cors = require('cors')
@@ -11,22 +11,33 @@ const port = config.port || 4000
 app.use(cors())
 app.use(helmet())
 
-const pool = mysql.createPool(config.db)
-
-app.get('/', (req, res) => {
-  pool.getConnection((err, connection) => {
+app.get('/', async (req, res) => {
+  
+	try {
+		const pool = await mysql.createPool(config.db)
+		const connection = await pool.getConnection()
+		const [rows] = await connection.execute('SELECT * FROM lockers WHERE floor = ?', [5])
+		res.status(200).send(rows)
+		await connection.release()
+	} catch (err) {
+		res.status(300).json({'error':'sorry, an error has occured'})
+		console.error(err)
+	}
+	
+	/*pool.getConnection((err, connection) => {
     if (err) console.error(err)
-    connection.query('SELECT * ', (err, results, fields) => {
+		// .execute creates a prepared statement
+    connection.execute('SELECT * FROM lockers WHERE floor = ?', [5], (err, results) => {
       if (err) {
 				res.status(300).json({'error':'sorry, an error has occured'})
 				console.error(err)
 			}
-      res.status(200).send(results)
+      res.status(200).send(results )
     })
-    connection.release()
-  })
+    pool.releaseConnection(connection)
+  })*/
 })
 
 app.listen(port, () => {
-  console.log(`server listening on ${port}`)
+  console.log(`########################\nserver listening on ${port}\n########################`)
 })
