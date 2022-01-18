c3fc7f4cd187ce82400bc368c21ee39f502159a0
Tyler Jones <tjones357@gatech.edu>
2022-01-18 14:42:56 -0500

    add basic edit button to row

--- frontend/src/components/LockerTable.vue
+++ frontend/src/components/LockerTable.vue
@@ -1,11 +1,32 @@
 <template>
   <b-table
     :data="filterLockers"
-    :columns="columns"
     :paginated="isPaginated"
     :per-page="perPage"
     :current-page.sync="currentPage"
-  />
+  >
+    <b-table-column field="id" label="ID" width="40" :td-attrs="columnTdAttrs" numeric v-slot="props">
+      {{props.row.id}}
+    </b-table-column>
+    <b-table-column field="locker_number" label="Locker Number"  :td-attrs="columnTdAttrs" numeric v-slot="props">
+      {{props.row.locker_number}}
+    </b-table-column>
+    <b-table-column field="patron_name" label="Patron Name" :td-attrs="columnTdAttrs" searchable v-slot="props">
+      {{props.row.patron_name}}
+    </b-table-column>
+    <b-table-column field="building" label="Building" :td-attrs="columnTdAttrs" v-slot="props">
+      {{props.row.building}}
+    </b-table-column>
+    <b-table-column field="floor" label="Floor" :td-attrs="columnTdAttrs" numeric v-slot="props">
+      {{props.row.floor}}
+    </b-table-column>
+    <b-table-column field="locker_group" label="Locker Group" :td-attrs="columnTdAttrs" v-slot="props">
+      {{props.row.locker_group}}
+    </b-table-column>
+    <b-table-column :td-attrs="columnTdAttrs">
+      <b-button>Edit</b-button>
+    </b-table-column>
+  </b-table>
 </template>
 <script>
 export default {
@@ -26,37 +47,6 @@ export default {
       isPaginated: true,
       perPage: 20,
       currentPage: 1,
-      columns: [
-        {
-          field: 'id',
-          label: 'ID',
-          numeric: true,
-          width: '40'
-        },
-        {
-          field: 'locker_number',
-          label: 'Locker Number',
-          numeric: true
-        },
-        {
-          field: 'patron_name',
-          label: 'Patron',
-          searchable: true
-        },
-        {
-          field: 'building',
-          label: 'Building'
-        },
-        {
-          field: 'floor',
-          label: 'Floor',
-          numeric: true
-        },
-        {
-          field: 'locker_group',
-          label: 'Locker Group'
-        }
-      ]
     }
   },
   computed: {
