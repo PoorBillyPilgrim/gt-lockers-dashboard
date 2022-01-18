608b7fa299cd9954056f87d3613c3007c0b1b07b
Tyler Jones <tjones357@gatech.edu>
2022-01-18 16:49:03 -0500

    add modal card with save and delete buttons

--- frontend/src/components/LockerTable.vue
+++ frontend/src/components/LockerTable.vue
@@ -1,44 +1,102 @@
 <template>
   <div>
     <b-table
-        :data="filterLockers"
-        :striped="true"
-        :hoverable="true"
-        :paginated="isPaginated"
-        :per-page="perPage"
-        :current-page.sync="currentPage"
+      :data="filterLockers"
+      :striped="true"
+      :hoverable="true"
+      :paginated="isPaginated"
+      :per-page="perPage"
+      :current-page.sync="currentPage"
+    >
+      <b-table-column
+        v-slot="props"
+        field="id"
+        label="ID"
+        width="40"
+        numeric
       >
-    <b-table-column field="id" label="ID" width="40" numeric v-slot="props">
-      {{props.row.id}}
-    </b-table-column>
-    <b-table-column field="locker_number" label="Locker Number"  numeric v-slot="props">
-      {{props.row.locker_number}}
-    </b-table-column>
-    <b-table-column field="patron_name" label="Patron Name" searchable v-slot="props">
-      {{props.row.patron_name}}
-    </b-table-column>
-    <b-table-column field="building" label="Building" v-slot="props">
-      {{props.row.building}}
-    </b-table-column>
-    <b-table-column field="floor" label="Floor" numeric v-slot="props">
-      {{props.row.floor}}
-    </b-table-column>
-    <b-table-column field="locker_group" label="Locker Group" v-slot="props">
-      {{props.row.locker_group}}
-    </b-table-column>
-    <b-table-column v-slot="props">
-      <b-button type="is-success" @click="edit(props.row.id)">Edit</b-button>
-    </b-table-column>
-  </b-table>
-  <b-modal v-model="isEditModalActive" :locker="currentLocker" :width="500">
-    <div class="card">
-      <div class="card-content">
-        <div class="content">
-          {{currentLocker}}
-        </div>
+        {{ props.row.id }}
+      </b-table-column>
+      <b-table-column
+        v-slot="props"
+        field="locker_number"
+        label="Locker Number"
+        numeric
+      >
+        {{ props.row.locker_number }}
+      </b-table-column>
+      <b-table-column
+        v-slot="props"
+        field="current_code"
+        label="Current Code"
+        numeric
+      >
+        {{ props.row.current_code }}
+      </b-table-column>
+      <b-table-column
+        v-slot="props"
+        field="patron_name"
+        label="Patron Name"
+        searchable
+      >
+        {{ props.row.patron_name }}
+      </b-table-column>
+      <b-table-column
+        v-slot="props"
+        field="building"
+        label="Building"
+      >
+        {{ props.row.building }}
+      </b-table-column>
+      <b-table-column
+        v-slot="props"
+        field="floor"
+        label="Floor"
+        numeric
+      >
+        {{ props.row.floor }}
+      </b-table-column>
+      <b-table-column
+        v-slot="props"
+        field="locker_group"
+        label="Locker Group"
+      >
+        {{ props.row.locker_group }}
+      </b-table-column>
+      <b-table-column v-slot="props">
+        <b-button
+          type="is-success"
+          @click="edit(props.row.id)"
+        >
+          Edit
+        </b-button>
+      </b-table-column>
+    </b-table>
+    <b-modal
+      v-model="isEditModalActive"
+      has-modal-card
+      :locker="currentLocker"
+    >
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">
+            Locker #{{ currentLocker.locker_number }}, {{ currentLocker.building }}
+          </p>
+        </header>
+        <section class="modal-card-body">
+          <p>Patron: {{ currentLocker.patron_name }}</p>
+          <p>Code: {{ currentLocker.current_code }}</p>
+        </section>
+        <footer class="modal-card-foot">
+          <b-button type="is-success">
+            Save
+          </b-button>
+          <b-button type="is-danger">
+            Cancel
+          </b-button>
+        </footer>
       </div>
-    </div>
-  </b-modal>
+    </b-modal>
   </div>
 </template>
 <script>
@@ -61,14 +119,8 @@ export default {
       isPaginated: true,
       perPage: 20,
       currentPage: 1,
-      currentLocker: null,
+      currentLocker: {},
       isEditModalActive: false
-    }
-  },
-  methods: {
-    edit(id) {
-      this.isEditModalActive = true
-      this.currentLocker = this.lockers.find(locker => locker.id === id)
     }
   },
   computed: {
@@ -77,6 +129,12 @@ export default {
         return this.lockers.filter(locker => locker.locker_group === this.group)
       }
       return this.lockers
+    }
+  },
+  methods: {
+    edit(id) {
+      this.isEditModalActive = true
+      this.currentLocker = this.lockers.find(locker => locker.id === id)
     }
   }
 }
