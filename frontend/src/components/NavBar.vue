e405ee42ac21668a487781f357d86bcfb9d5bc5e
Tyler Jones <tjones357@gatech.edu>
2022-01-24 11:23:53 -0500

    center navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar :shadow="true" :spaced="true">
+  <b-navbar :shadow="true" :spaced="true" :centered="true">
     <template #start>
       <b-navbar-item
         tag="router-link"
