a3ca6f5bec49fa5056ed8d8f0dfd129dd73c6e98
Tyler Jones <tjones357@gatech.edu>
2022-02-01 17:16:37 -0500

    make login a modal card

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,30 +1,39 @@
 <template>
-  <form
-    class="box"
-    @submit.prevent="login"
+  <b-modal
+    :active="isActive"
+    :can-cancel="false"
+    has-modal-card
   >
-    <b-field label="Email">
-      <b-input
-        v-model="email"
-        placeholder="Please enter your GT email"
-      />
-    </b-field>
-    <b-field label="Password">
-      <b-input
-        v-model="password"
-        type="password"
-      />
-    </b-field>
-    <b-button native-type="submit">
-      Login
-    </b-button>
-  </form>
+    <form @submit.prevent="login" class="modal-card">
+      <header class="modal-card-head">Login</header>
+      <section class="modal-card-body">
+        <b-field label="Email">
+          <b-input
+            v-model="email"
+            placeholder="Please enter your GT email"
+          />
+        </b-field>
+        <b-field label="Password">
+          <b-input
+            v-model="password"
+            type="password"
+          />
+        </b-field>
+      </section>
+      <footer class="modal-card-foot">
+        <b-button native-type="submit">
+          Login
+        </b-button>
+      </footer>
+    </form>
+  </b-modal>
 </template>
 <script>
 export default {
   name: 'Login',
   data() {
     return {
+      isActive: true,
       email: '',
       password: ''
     }
@@ -32,6 +41,7 @@ export default {
   methods: {
     login() {
       if (this.email === 'test@test.com' && this.password === 'test') {
+        this.isActive = false
         this.$emit('login')
       }
     }
