c22310aea7da3d863d42778ce4677c4c5f569d75
Tyler Jones <tjones357@gatech.edu>
2022-02-01 11:50:43 -0500

    render simple login form on /login route

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,8 +1,28 @@
 <template>
-    
+  <form
+    class="box"
+    @submit.prevent="submit"
+  >
+    <b-field label="Email">
+      <b-input value="Please enter your GT email" />
+    </b-field>
+    <b-field label="Password">
+      <b-input
+        type="password"
+      />
+    </b-field>
+    <b-button native-type="submit">
+      Login
+    </b-button>
+  </form>
 </template>
 <script>
 export default {
-    
+  name: 'Login',
+  methods: {
+    submit() {
+      console.log('submitted!')
+    }
+  }
 }
 </script>
