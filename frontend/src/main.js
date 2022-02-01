f02b5c36dc4f4183bdd75872a5be2a40fb72a2af
Tyler Jones <tjones357@gatech.edu>
2022-02-01 11:52:44 -0500

    add /login route and change VueRouter to use history mode

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/components/Login.vue'
 import LockerTabs from '@/components/LockerTabs.vue'
 import LockersAssign from '@/components/LockersAssign.vue'
+// import Logout from '@/component/Logout.vue'
 import Buefy from 'buefy'
 import App from './App.vue'
 
@@ -11,6 +13,10 @@ Vue.use(Buefy)
 Vue.config.productionTip = false
 
 const routes = [
+  {
+    path: '/login',
+    component: Login
+  },
   { 
     path: '/lockers', 
     component: LockerTabs,
@@ -21,10 +27,16 @@ const routes = [
     component: LockersAssign,
     props: { title: "Find Available Locker"}
   },
+  // {
+  //   path: '/logout', component: Logout
+  // },
   { path: '*', redirect: '/lockers' }
 ]
 
-const router = new VueRouter({ routes })
+const router = new VueRouter({ 
+  mode: 'history',
+  routes: routes 
+})
 
 new Vue({
   render: h => h(App),
