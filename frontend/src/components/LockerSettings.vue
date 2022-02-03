e68ddcd34ac7b9ecd3aea69f559b34b3544c5238
Tyler Jones <tjones357@gatech.edu>
2022-02-03 13:13:56 -0500

    init LockerSettings tab

---
+++ frontend/src/components/LockerSettings.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1 class="title has-text-centered">
+      {{ title }}
+    </h1>
+      <div v-if="isLoading">
+          <b-loading v-model="isLoading"/>
+      </div>
+      <div class="box" v-else>
+        <b-field label="Status">
+            <b-select
+            v-model="schema.locker_status"
+            :placeholder="schema.locker_status"
+            >
+            <option 
+                v-for="(status,index) in schema.locker_status" 
+                :key="index"
+                :value="status"
+            >
+                {{ status }}
+            </option>
+            </b-select>
+        </b-field>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LockerSettings',
+  props: {
+    title: {
+      type: String,
+      default: 'Lockers'
+    },
+    schema: {
+      type: Object,
+      default: () => {}
+    },
+    isLoading: {
+      type: Boolean
+    }
+  }
+}
+</script>
