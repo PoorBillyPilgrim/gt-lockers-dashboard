b1e32b1e13a407f1334c1c7222796f43325e0311
Tyler Jones <tjones357@gatech.edu>
2022-02-14 10:30:44 -0500

    init locker settings

--- frontend/src/components/LockerSettings.vue
+++ frontend/src/components/LockerSettings.vue
@@ -3,11 +3,45 @@
     <h1 class="title has-text-centered">
       {{ title }}
     </h1>
-      <div v-if="isLoading">
-          <b-loading v-model="isLoading"/>
+    <div v-if="isLoading">
+      <b-loading v-model="isLoading" />
+    </div>
+    <div
+      v-else
+      class="box"
+    >
+    <div class="columns is-justify-content-space-between">
+      <div class="column">
+        <h2 class="title">
+          Locker Status
+        </h2>
+        <h3 class="subtitle">
+          Add or remove different status options
+        </h3>
       </div>
-      <div class="box" v-else>
-        <b-field label="Status">
+      <div class="column is-flex-grow-0">
+        <b-button>Add</b-button>
+      </div>
+    </div>
+
+      <b-table
+        :data="lockerStatuses"
+      >
+        <b-table-column
+          v-slot="props"
+          field="locker_status"
+          label="Locker Status"
+        >
+        {{props.row.locker_status}}
+        </b-table-column>
+        <b-table-column>
+          <b-button type="is-primary">Edit</b-button>
+        </b-table-column>
+        <b-table-column>
+          <b-button type="is-danger">Delete</b-button>
+        </b-table-column>
+      </b-table>
+      <!--<b-field label="Status">
             <b-select
             v-model="schema.locker_status"
             :placeholder="schema.locker_status"
@@ -20,8 +54,8 @@
                 {{ status }}
             </option>
             </b-select>
-        </b-field>
-      </div>
+        </b-field>-->
+    </div>
   </div>
 </template>
 <script>
@@ -38,6 +72,27 @@ export default {
     },
     isLoading: {
       type: Boolean
+    }
+  },
+  data() {
+    return {
+      columns: [
+        {
+          field: 'locker_status',
+          label: 'Locker Status'
+        }
+      ]
+    }
+  },
+  computed: {
+    lockerStatuses: function() {
+      const arr = []
+      this.schema.locker_status.forEach(status => {
+        arr.push({
+          "locker_status": status
+        })
+      })
+      return arr
     }
   }
 }
